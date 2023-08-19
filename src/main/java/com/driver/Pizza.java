c1882f1175c424edbda7863ac16b993e317447c1
Ashutosh <ashushukla800@gmail.com>
2023-08-19 17:46:53 +0530

    last

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -1,3 +1,4 @@
+
 package com.driver;
 
 public class Pizza {
@@ -5,59 +6,64 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
+
     private boolean cheeseAdded;
     private boolean toppingsAdded;
     private boolean bagAdded;
     private boolean billGenerated;
 
+
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
-        if(this.isVeg){
+        if(this.isVeg==true){
             this.price+=300;
             this.bill="Base Price Of The Pizza: 300\n";
-        }
-        else{
+        }else{
             this.price+=400;
             this.bill="Base Price Of The Pizza: 400\n";
         }
     }
 
     public int getPrice(){
-
         return this.price;
     }
 
     public void addExtraCheese(){
-        // your code goes here
+//        if(this.bill.contains("Extra Cheese Added:")){
+//            return;
+//        }
         if(this.cheeseAdded){
             return;
         }
+        // your code goes here
         this.price+=80;
         this.cheeseAdded=true;
         this.bill+="Extra Cheese Added: 80\n";
     }
 
     public void addExtraToppings(){
-        // your code goes here
+//        if(this.bill.contains("Extra Toppings Added:")){
+//            return;
+//        }
         if(this.toppingsAdded){
             return;
         }
         this.toppingsAdded=true;
-
-           if(this.isVeg){
-               this.price+=70;
-               this.bill="Extra Toppings Added: 70\n";
-           }
-           else{
-               this.price+=120;
-               this.bill="Extra Toppings Added: 120\n";
-           }
-
+        // your code goes here
+        if(this.isVeg==true){
+            this.price+=70;
+            this.bill+="Extra Toppings Added: 70\n";
+        }else{
+            this.bill+="Extra Toppings Added: 120\n";
+            this.price+=120;
+        }
     }
 
     public void addTakeaway(){
-        // your code goes here
+//        if(this.bill.contains("Paperbag Added:")){
+//            return;
+//        }
         if(this.bagAdded){
             return;
         }
@@ -69,6 +75,9 @@ public class Pizza {
 
     public String getBill(){
         // your code goes here
+//        if(this.bill.contains("Total Price:")){
+//            return this.bill;
+//        }
         if(this.billGenerated){
             return this.bill;
         }
